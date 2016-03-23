4db7b261cf1eaf1eaaa567cada1777960f68cfc4
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2016-03-23 16:02:50 +1100

    Initial Commit

---
+++ calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  calculator
+//
+//  Created by Brokmeier, Pascal on 23/03/16.
+//  Copyright © 2016 Pascal Brokmeier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
